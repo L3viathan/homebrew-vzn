18c53de84d118aa3918afeaaec74dcbb5c96b9f3
L3viathan <git@l3vi.de>
2022-05-17 22:06:46 +0200

    Add dependencies

--- Formula/aceto.rb
+++ Formula/aceto.rb
@@ -9,6 +9,16 @@ class Aceto < Formula
 
   depends_on "python"
 
+  resource "click" do
+    url "https://files.pythonhosted.org/packages/27/6f/be940c8b1f1d69daceeb0032fee6c34d7bd70e3e649ccac0951500b4720e/click-7.1.2.tar.gz"
+    sha256 "d2b5255c7c6349bc1bd1e59e08cd12acbbd63ce649f2588755783aa94dfb6b1a"
+  end
+
+  resource "hilbertcurve" do
+    url "https://files.pythonhosted.org/packages/b3/d1/0ee30ca7b861c3396759ee47a6dd997806f1c65314b4782518bdc019d6da/hilbertcurve-1.0.5.tar.gz"
+    sha256 "aefc00859c9cdefee1e53b722c33b2091afa18a608a7eedc7ee2acdbf62feb36"
+  end
+
   def install
     # ENV.deparallelize  # if your formula fails when building in parallel
     virtualenv_install_with_resources
